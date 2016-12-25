986b39b8635f5b0e897fea1635bb1c6dca83d25d
badlogic <contact@badlogicgames.com>
2016-12-26 00:12:53 +0100

    Formatting

--- kek/src/main/kotlin/kek/Ast.kt
+++ kek/src/main/kotlin/kek/Ast.kt
@@ -78,35 +78,30 @@ class FieldAccess(val base: Expression, val varName: Token) : Expression() {
 class FunctionCall(val function: Expression, val arguments: List<Expression>) : Expression() {
 }
 
-class MethodCall(val base: Expression, val methodName: Token, val arguments: List<Expression>) : Expression() {
-}
-
 class VariableDeclaration(val name: Token, val type: Type, val initializer: Expression) : Statement() {
 }
 
-var i = 0;
+var i = 0
 
 fun printAst(cu: CompilationUnit): String {
-    var buffer = StringBuffer();
-    var nodes = StringBuffer();
-    var edges = StringBuffer();
+    val buffer = StringBuffer()
+    val nodes = StringBuffer()
+    val edges = StringBuffer()
 
-    buffer.append("digraph cu {\n");
-    nodes.append("node[shape=box]\n");
-    nodes.append("cu [label=\"Compilation Unit\", shape=box]\n");
-    nodes.append("ns [label=\"Namespace: ${cu.nameSpace}\"]\n");
-    edges.append("cu -> ns\n");
+    buffer.append("digraph cu {\n")
+    nodes.append("node[shape=box]\n")
+    nodes.append("cu [label=\"Compilation Unit\", shape=box]\n")
+    nodes.append("ns [label=\"Namespace: ${cu.nameSpace}\"]\n")
+    edges.append("cu -> ns\n")
 
-
-    var i = 0;
     for (f in cu.functions) {
-        printAstNode("cu", f, nodes, edges);
+        printAstNode("cu", f, nodes, edges)
     }
 
-    buffer.append(nodes);
-    buffer.append(edges);
-    buffer.append("}\n");
-    return buffer.toString();
+    buffer.append(nodes)
+    buffer.append(edges)
+    buffer.append("}\n")
+    return buffer.toString()
 }
 
 fun printAstNode(p: String, n: AstNode, nodes: StringBuffer, edges: StringBuffer): String {
@@ -130,49 +125,49 @@ fun printAstNode(p: String, n: AstNode, nodes: StringBuffer, edges: StringBuffer
     else if (n is DoStatement) return printDoStatement(p, n, nodes, edges)
     else if (n is ReturnStatement) return printReturnStatement(p, n, nodes, edges)
     else if (n is TernaryOperator) return printTernaryOperator(p, n, nodes, edges)
-    else if (n is EmptyExpression) return "empty";
-    else throw RuntimeException("Unknown AST node $n");
+    else if (n is EmptyExpression) return "empty"
+    else throw RuntimeException("Unknown AST node $n")
 }
 
 fun  printWhileStatement(p: String, n: WhileStatement, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"While\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.condition, nodes, edges);
-    printAstNodeList(name, n.body, nodes, edges);
-    return name;
+    printAstNode(name, n.condition, nodes, edges)
+    printAstNodeList(name, n.body, nodes, edges)
+    return name
 }
 
 fun  printDoStatement(p: String, n: DoStatement, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"Do\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.condition, nodes, edges);
-    printAstNodeList(name, n.body, nodes, edges);
-    return name;
+    printAstNode(name, n.condition, nodes, edges)
+    printAstNodeList(name, n.body, nodes, edges)
+    return name
 }
 
 fun printForStatement(p: String, n: ForStatement, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"For\"]\n")
     edges.append("$p->$name\n")
-    printAstNodeList(name, n.initializer, nodes, edges);
-    printAstNode(name, n.condition, nodes, edges);
-    printAstNodeList(name, n.increment, nodes, edges);
-    printAstNodeList(name, n.body, nodes, edges);
-    return name;
+    printAstNodeList(name, n.initializer, nodes, edges)
+    printAstNode(name, n.condition, nodes, edges)
+    printAstNodeList(name, n.increment, nodes, edges)
+    printAstNodeList(name, n.body, nodes, edges)
+    return name
 }
 
 fun printReturnStatement(p: String, n: ReturnStatement, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"Return\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.expression, nodes, edges);
-    return name;
+    printAstNode(name, n.expression, nodes, edges)
+    return name
 }
 
 fun printAstNodeList(p: String, l: List<AstNode>, nodes: StringBuffer, edges: StringBuffer): String {
-    var last = p;
+    var last = p
     for (s in l) {
         last = printAstNode(last, s, nodes, edges)
     }
@@ -183,38 +178,38 @@ fun printIfStatement(p: String, n: IfStatement, nodes: StringBuffer, edges: Stri
     val name = "b${i++}"
     nodes.append("$name [label=\"If\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.condition, nodes, edges);
-    printAstNodeList(name, n.trueBody, nodes, edges);
+    printAstNode(name, n.condition, nodes, edges)
+    printAstNodeList(name, n.trueBody, nodes, edges)
     printAstNodeList(name, n.elseIfs, nodes, edges)
-    printAstNodeList(name, n.falseBody, nodes, edges);
-    return name;
+    printAstNodeList(name, n.falseBody, nodes, edges)
+    return name
 }
 
 fun printTernaryOperator(p: String, n: TernaryOperator, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"Ternary Operator\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.left, nodes, edges);
-    printAstNode(name, n.middle, nodes, edges);
-    printAstNode(name, n.right, nodes, edges);
-    return name;
+    printAstNode(name, n.left, nodes, edges)
+    printAstNode(name, n.middle, nodes, edges)
+    printAstNode(name, n.right, nodes, edges)
+    return name
 }
 
 fun printVariableDeclaration(p: String, n: VariableDeclaration, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"Variable ${n.name.text}, ${n.type.identifier?.text}\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.initializer, nodes, edges);
-    return name;
+    printAstNode(name, n.initializer, nodes, edges)
+    return name
 }
 
 fun printFunctionCall(p: String, n: FunctionCall, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "b${i++}"
     nodes.append("$name [label=\"Call ()\"]\n")
     edges.append("$p->$name\n")
-    printAstNode(name, n.function, nodes, edges);
+    printAstNode(name, n.function, nodes, edges)
     for (a in n.arguments) printAstNode(name, a, nodes, edges)
-    return name;
+    return name
 }
 
 fun printFieldAccess(p: String, n: FieldAccess, nodes: StringBuffer, edges: StringBuffer): String {
@@ -222,7 +217,7 @@ fun printFieldAccess(p: String, n: FieldAccess, nodes: StringBuffer, edges: Stri
     nodes.append("$name [label=\"Field Access .${n.varName.text}\"]\n")
     edges.append("$p->$name\n")
     printAstNode(name, n.base, nodes, edges)
-    return name;
+    return name
 }
 
 fun printArrayAccess(p: String, n: ArrayAccess, nodes: StringBuffer, edges: StringBuffer): String {
@@ -231,42 +226,42 @@ fun printArrayAccess(p: String, n: ArrayAccess, nodes: StringBuffer, edges: Stri
     edges.append("$p->$name\n")
     printAstNode(name, n.array, nodes, edges)
     printAstNode(name, n.index, nodes, edges)
-    return name;
+    return name
 }
 
 fun printVariableAccess(p: String, n: VariableAccess, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "s${i++}"
     nodes.append("$name [label=\"Variable access '${n.varName.text}'\"]\n")
     edges.append("$p->$name\n")
-    return name;
+    return name
 }
 
 fun printBooleanLiteral(p: String, n: BooleanLiteral, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "s${i++}"
     nodes.append("$name [label=\"Boolean '${n.literal.text}'\"]\n")
     edges.append("$p->$name\n")
-    return name;
+    return name
 }
 
 fun printNumberLiteral(p: String, n: NumberLiteral, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "s${i++}"
     nodes.append("$name [label=\"Number '${n.literal.text}'\"]\n")
     edges.append("$p->$name\n")
-    return name;
+    return name
 }
 
 fun printStringLiteral(p: String, n: StringLiteral, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "s${i++}"
     nodes.append("$name [label=\"String '${n.literal.text}'\"]\n")
     edges.append("$p->$name\n")
-    return name;
+    return name
 }
 
 fun printCharacterLiteral(p: String, n: CharacterLiteral, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "s${i++}"
     nodes.append("$name [label=\"Character '${n.literal.text}'\"]\n")
     edges.append("$p->$name\n")
-    return name;
+    return name
 }
 
 fun printBinaryOperator(p: String, n: BinaryOperator, nodes: StringBuffer, edges: StringBuffer): String {
@@ -275,7 +270,7 @@ fun printBinaryOperator(p: String, n: BinaryOperator, nodes: StringBuffer, edges
     edges.append("$p->$name\n")
     printAstNode(name, n.left, nodes, edges)
     printAstNode(name, n.right, nodes, edges)
-    return name;
+    return name
 }
 
 fun printUnaryOperator(p: String, n: UnaryOperator, nodes: StringBuffer, edges: StringBuffer): String {
@@ -283,19 +278,19 @@ fun printUnaryOperator(p: String, n: UnaryOperator, nodes: StringBuffer, edges: 
     nodes.append("$name [label=\"Unary ${n.opType.text}\"]\n")
     edges.append("$p->$name\n")
     printAstNode(name, n.expr, nodes, edges)
-    return name;
+    return name
 }
 
 fun printParameter(p: String, n: Parameter, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "p${i++}"
     nodes.append("$name [label=\"Param: ${n.identifier.text}, ${n.type.identifier?.text}\"]\n")
     edges.append("$p->$name\n")
-    return name;
+    return name
 }
 
 fun printStructureDefinition(n: StructureDefinition, nodes: StringBuffer, edges: StringBuffer): String {
     val name = "s${i++}"
-    return name;
+    return name
 }
 
 fun printFunctionDefinition(n: FunctionDefinition, nodes: StringBuffer, edges: StringBuffer): String {
@@ -304,7 +299,7 @@ fun printFunctionDefinition(n: FunctionDefinition, nodes: StringBuffer, edges: S
     edges.append("cu->$name\n")
     for (p in n.parameters) printAstNode(name, p, nodes, edges)
 
-    var last = name;
+    var last = name
     for (s in n.body) {
         last = printAstNode(last, s, nodes, edges)
     }
